9f479b0d4762040360feb951071c48b0cc84b4aa
James Chan <jwchan1993@gmail.com>
2018-12-09 22:34:17 +0000

    Fixed issues

--- themes/hugo-creative-theme/static/js/script.js
+++ themes/hugo-creative-theme/static/js/script.js
@@ -1,5 +1,14 @@
-let aboutButton = document.querySelector("#our-founder");
+let aboutButton = document.querySelector('#our-founder');
+let about = document.querySelector('#founder');
+
+//about.style.display = 'none';
 
 aboutButton.addEventListener('click', () => {
-
+    /*if(about.style.display === 'none'){
+        about.style.display = 'block';
+    }
+    else{
+        about.style.display = 'none';
+    }*/
+    console.log('Hello');
 });
